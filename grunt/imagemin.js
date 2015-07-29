adb78723ac0f9a20a1863c93ed1f79fe2e401de6
Chris Sargent <chris@stickypixel.com>
2015-07-29 21:21:39 +0200

    remove grunt folder

--- grunt/imagemin.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "files": {
-        "expand": true,
-        "cwd": "",
-        "src": ["<%= global.imagesPath %>**/*.{png,jpg,gif}"],
-        "dest": "<%= global.buildPath %>"
-    }
-};
--- grunt/uglify.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    
-    "build": {
-        "src": "<%= global.customJS %>",
-        "dest": "<%= global.buildPath %><%= global.outputMinJS %>"
-    }
-
-};
